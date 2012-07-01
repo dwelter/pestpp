f5dd42142e923cee261f768935d5cd0f0d3e24e5
dwelter <dwelter1@bellsouth.net>
2012-07-01 10:03:35 -0400

    Refactored SVDSolver and move  Parameters from constructor to Solve method

--- pest++/pest++/ModelRunPP.h
+++ pest++/pest++/ModelRunPP.h
@@ -28,7 +28,6 @@
 #include "ParamTransformSeq.h"
 
 using namespace std;
-class ModelRunShallowCopy;
 
 class ModelRunAbstractBase
 {
@@ -69,13 +68,9 @@ public:
 class ModelRun : public ModelRunAbstractBase
 {
 public:
-	friend class ModelRunShallowCopy;
-	enum CopyType {DeepCopy, ShallowCopy};
-	//ModelRun(const ModelRun &rhs, CopyType=DeepCopy);
 	ModelRun(const ObjectiveFunc *_objectiveFunc, const ParamTransformSeq &_par_tran, Observations _sim_obs);
 	ModelRun(const ObjectiveFunc *_objectiveFunc, const ParamTransformSeq &_par_tran, const Parameters &_numeric_pars, const Observations &_sim_obs);
 	ModelRun& operator=(const ModelRun &rhs);
-	//const ModelRun& operator=(const ModelRunShallowCopy &rhs);
 	virtual void set_numeric_parameters(const Parameters &pars);
 	virtual void set_ctl_parameters(const Parameters &pars);
 	virtual void set_model_parameters(const Parameters &pars);
@@ -111,46 +106,5 @@ protected:
 private:
 	ParamTransformSeq par_tran;
 };
-
-//class ModelRunShallowCopy  : public ModelRunAbstractBase
-//{
-//public:
-//	friend class ModelRun;
-//	ModelRunShallowCopy(const ModelRun &model_run);
-//	//ModelRunShallowCopy(const ModelRunShallowCopy &model_run);
-//	//const ModelRunShallowCopy& operator=(const ModelRunShallowCopy &rhs);
-//	const ModelRunShallowCopy& ModelRunShallowCopy::operator=(const ModelRun &rhs);
-//	virtual void set_numeric_parameters(const Parameters &pars);
-//	virtual void set_ctl_parameters(const Parameters &pars);
-//	virtual void set_model_parameters(const Parameters &pars);
-//	virtual void set_observations(const Observations &observations);
-//	virtual const Parameters &get_numeric_pars();
-//	virtual Parameters get_ctl_pars();
-//	virtual const Parameters &get_model_pars();
-//	virtual const Observations &get_obs();
-//	virtual Observations get_obs_template() const;
-//	virtual const ParamTransformSeq &get_par_tran()  {return *(base_model_run_ptr->par_tran);}
-//	virtual double get_phi(double regul_weight=1.0);
-//	virtual PhiComponets get_phi_comp();
-//	virtual vector<double> get_residuals_vec(const vector<string> &obs_names);
-//	virtual void phi_report(ostream &os);
-//	virtual bool obs_valid() const;
-//	virtual bool numeric_pars_valid() const;
-//	virtual bool model_pars_valid() const;
-//	virtual bool phi_valid() const;
-//	const ModelRun *get_base_model_run_ptr()const;
-//	virtual ~ModelRunShallowCopy(){};
-//private:
-//	const ModelRun *base_model_run_ptr;
-//	Parameters numeric_pars;
-//	Parameters model_pars;
-//	Observations sim_obs;
-//	PhiComponets phi_comp;
-//	bool numeric_pars_is_valid;
-//	bool model_pars_is_valid;
-//	bool obs_is_valid;
-//	bool phi_is_valid;
-//};
-
 
 #endif /* MODELRUNPP_H_ */
