a1db3b346cbec9fff8ed9355d9d6ee374130bcc8
Dave Welter <dwelter1@bellsouth.net>
2014-03-30 20:10:57 -0400

    Version 2.2.1

--- pest++/pest++/pest++.cpp
+++ pest++/pest++/pest++.cpp
@@ -50,7 +50,7 @@ using namespace pest_utils;
 
 int main(int argc, char* argv[])
 {
-	string version = "2.2.0";
+	string version = "2.2.1";
 	cout << endl << endl;
 	cout << "             PEST++ Version " << version << endl << endl;
 	cout << "                 by Dave Welter" << endl;
