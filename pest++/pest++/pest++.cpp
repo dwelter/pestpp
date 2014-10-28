230c8d0fd37f12ab2988b600c11b0eb4fc9476f6
Dave Welter <dwelter1@bellsouth.net>
2014-10-27 20:41:45 -0400

    Version 3.0.0 official release

--- pest++/pest++/pest++.cpp
+++ pest++/pest++/pest++.cpp
@@ -58,7 +58,7 @@ int main(int argc, char* argv[])
 	try
 	{
 #endif
-		string version = "3.0.0_rc4";
+		string version = "3.0.0";
 		cout << endl << endl;
 		cout << "             PEST++ Version " << version << endl << endl;
 		cout << "                 by Dave Welter" << endl;
