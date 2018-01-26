bug fix when using prior scaling, check in before rerunning tests

benchmarks/ies_test.py
@@ -438,65 +438,102 @@ def test_synth():
 
     if os.path.exists(test_d):
         shutil.rmtree(test_d)
-    #shutil.copytree(template_d,test_d)
-    # print("loading pst")
-    # pst = pyemu.Pst(os.path.join(template_d,"pest.pst"))
-    # pst.pestpp_options = {}
-    # pst.pestpp_options["ies_num_reals"] = 30
-    # pst.control_data.noptmax = 1
-    # print("writing pst")
-    # pst.write(os.path.join(template_d,"pest.pst"))
+    print("loading pst")
+    pst = pyemu.Pst(os.path.join(template_d,"pest.pst"))
+    pst.pestpp_options = {}
+    pst.pestpp_options["ies_num_reals"] = 30
+    pst.pestpp_options["ies_use_approx"] = "false"
+    pst.pestpp_options["ies_use_prior_scaling"] = "true"
+    pst.control_data.noptmax = 3
+    print("writing pst")
+    pst.write(os.path.join(template_d,"pest.pst"))
     print("starting slaves")
     pyemu.helpers.start_slaves(template_d,exe_path,"pest.pst",num_slaves=6,master_dir=test_d,slave_root=model_d)
 
-def chenoliver_test():
+def test_chenoliver():
     model_d = "ies_chenoliver"
     test_d = os.path.join(model_d,"master")
     template_d = os.path.join(model_d,"template")
+
+    # build the prior cov matrix
+    x = np.zeros((1,1)) + 0.5
+    cov = pyemu.Cov(x=x,names=["par"])
+    cov.to_ascii(os.path.join(template_d,"prior.cov"))
 
     if os.path.exists(test_d):
         shutil.rmtree(test_d)
     shutil.copytree(template_d,test_d)
     pst = pyemu.Pst(os.path.join(template_d,"pest.pst"))
+    pst.parameter_data.loc[:,"parlbnd"] = -1.0e+10
+    pst.parameter_data.loc[:,"parubnd"] = 1.0+10
+
     pst.pestpp_options = {}
+    pst.pestpp_options["parcov_filename"] = "prior.cov"
     pst.pestpp_options["ies_num_reals"] = 100
     pst.control_data.noptmax = 0
     pst.write(os.path.join(test_d,"pest.pst"))
     pyemu.helpers.run(exe_path+" pest.pst",cwd=test_d)
     
+    noptmax = 20
     shutil.rmtree(test_d)
     pst.pestpp_options = {}
+    pst.pestpp_options["parcov_filename"] = "prior.cov"
     pst.pestpp_options["ies_num_reals"] = 100
-    pst.pestpp_options["ies_lambda_mults"] = "0.01,1.0,100.0"
-    pst.pestpp_options["ies_initial_lambda"] = 0.1
-    pst.pestpp_options["ies_subset_size"] = 10
-    pst.control_data.noptmax = 3
+    #pst.pestpp_options["ies_lambda_mults"] = "0.01,1.0,100.0"
+    pst.pestpp_options["ies_initial_lambda"] = 0.001
+    #pst.pestpp_options["ies_subset_size"] = 10
+    pst.pestpp_options["ies_use_approx"] = "false"
+    pst.pestpp_options["ies_use_prior_scaling"] = "true"
+    pst.control_data.noptmax = noptmax
+    pst.write(os.path.join(template_d,"pest.pst"))
+    
+
+    pyemu.helpers.start_slaves(template_d,exe_path,"pest.pst",num_slaves=6,
+        master_dir=test_d,slave_root=model_d,port=4005)
+    df_full = pd.read_csv(os.path.join(test_d,"pest.phi.meas.csv"),index_col=0)
+
+    shutil.rmtree(test_d)
+    pst.pestpp_options = {}
+    pst.pestpp_options["parcov_filename"] = "prior.cov"
+    pst.pestpp_options["ies_num_reals"] = 100
+    #pst.pestpp_options["ies_lambda_mults"] = "0.01,1.0,100.0"
+    pst.pestpp_options["ies_initial_lambda"] = 0.001
+    #pst.pestpp_options["ies_subset_size"] = 10
+    pst.control_data.noptmax = noptmax
     pst.write(os.path.join(template_d,"pest.pst"))
 
     pyemu.helpers.start_slaves(template_d,exe_path,"pest.pst",num_slaves=6,
         master_dir=test_d,slave_root=model_d,port=4005)
+    df_approx = pd.read_csv(os.path.join(test_d,"pest.phi.meas.csv"),index_col=0)
+
+    ax = plt.subplot(111)
+    ax.plot(df_full.loc[:,"mean"],color='b',label="full")
+    ax.plot(df_approx.loc[:,"mean"],color='g',label="approx")
+    plt.savefig(os.path.join(model_d,"full_approx.png"))
+    plt.close("all")
 
 
 
 if __name__ == "__main__":
     # write_empty_test_matrix()
-    # setup_suite_dir("ies_freyberg")
-    # setup_suite_dir("ies_10par_xsec")
+    setup_suite_dir("ies_freyberg")
+    setup_suite_dir("ies_10par_xsec")
 
-    # run_suite("ies_freyberg")
-    # run_suite("ies_10par_xsec")
-    # rebase("ies_freyberg")
-    # rebase("ies_10par_xsec")
-    # tenpar_subset_test()
-    # tenpar_full_cov_test()
-    # test_synth()
-    # test_10par_xsec()
-    # test_freyberg()
-    # test_freyberg_full_cov()
+    run_suite("ies_freyberg")
+    run_suite("ies_10par_xsec")
+    rebase("ies_freyberg")
+    rebase("ies_10par_xsec")
+    tenpar_subset_test()
+    tenpar_full_cov_test()
+    test_freyberg_full_cov()
+    test_synth()
+    test_10par_xsec()
+    test_freyberg()
+    test_chenoliver()
     
     # invest()
-    # compare_suite("ies_10par_xsec")
-    # compare_suite("ies_freyberg")
+    compare_suite("ies_10par_xsec")
+    compare_suite("ies_freyberg")
     # tenpar_subset_test()
     # tenpar_full_cov_test()
-    chenoliver_test()
+   