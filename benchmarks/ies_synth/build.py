48fa79dc02c630d6aa00874e98355ae767c961e4
jtwhite1000 <jtwhite1000@gmail.com>
2018-02-02 08:47:11 +1300

    working on ies sythn prob on mac

--- benchmarks/ies_synth/build.py
+++ benchmarks/ies_synth/build.py
@@ -11,7 +11,7 @@ num_reals = 30
 # nlay, nrow, ncol = 116, 78, 59
 
 def setup():
-    run_fieldgen()
+    #run_fieldgen()
 
     nper = 1
     perlen = 100.0
@@ -70,6 +70,7 @@ def setup():
     # print(hk.min(), hk.max(), hk.mean(), hk.std())
     # hk = np.loadtxt("hk.dat")
     # vka = hk / 10.0
+    print(os.listdir('.'))
     hk = [np.loadtxt(os.path.join("truth_reals",f)) for f in os.listdir("truth_reals") if "real_" in f]
     vka = 10.**(np.random.normal(-1,0.25,(nlay,nrow,ncol)))
 
