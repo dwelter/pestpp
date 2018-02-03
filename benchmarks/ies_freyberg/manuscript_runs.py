075daddc439c1d216c8aefb81cff71b7509a1c49
jtwhite1000 <jtwhite1000@gmail.com>
2018-02-03 22:30:43 +1300

    more work in freyberg manu plots

--- benchmarks/ies_freyberg/manuscript_runs.py
+++ benchmarks/ies_freyberg/manuscript_runs.py
@@ -1,6 +1,10 @@
 import os
 import shutil
+import matplotlib
+font = {'size': 8}
+matplotlib.rc('font', **font)
 import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
 import pandas as pd
 import flopy
 import pyemu
@@ -70,34 +74,63 @@ def plot_domain():
 
 def plot_phi():
     master_dirs = [d for d in os.listdir('.') if "master_" in d and "pestpp" not in d]
-    fig = plt.figure(figsize=(6.5,3.5))
-    ax = plt.subplot(111)
+    fig = plt.figure(figsize=(4.5,4.5))
+    gs = gridspec.GridSpec(3,3)
+    ax = plt.subplot(gs[:-1,:])
+    ax1 = plt.subplot(gs[-1,0])
+    ax2 = plt.subplot(gs[-1,1])
+    ax3 = plt.subplot(gs[-1,-1])
+    # ax = plt.subplot2grid((3,3),(0,0),rowspan=3,colspan=2)
+    # ax2 = plt.subplot2grid((3,3),(0,0))
+    # ax3 = plt.subplot2grid((3, 3), (0, 1))
+    # ax3 = plt.subplot2grid((3, 3), (0, 2))
+    #ax = plt.subplot(111)
     #ax_ps = plt.subplot(132)
     #ax_nps = plt.subplot(133)
     #dfs = []
     cd = {30:'b',50:'g',100:'m'}
-    for master_dir in master_dirs:
-        phi_file = [f for f in os.listdir(master_dir) if "phi.actual" in f][0]
-        nr = int(master_dir.split('_')[1])
-        ps = master_dir.split('_')[-1]
-        df = pd.read_csv(os.path.join(master_dir,phi_file))
-        ls = '-'
-        label = "{0} reals, prior scaling".format(nr)
-        print(ps)
-        if ps == "nps":
-            ls = '--'
-            label = "{0} reals, no prior scaling".format(nr)
+    ad = {30:ax1,50:ax2,100:ax3}
+    #for master_dir in master_dirs:
+    for num_real in [30,50,100]:
+        for master_dir in master_dirs:
 
+            phi_file = [f for f in os.listdir(master_dir) if "phi.actual" in f][0]
+            nr = int(master_dir.split('_')[1])
+            if nr != num_real:
+                continue
+            ps = master_dir.split('_')[-1]
+            df = pd.read_csv(os.path.join(master_dir,phi_file))
+            ls = '-'
+            label = "{0} reals, prior scaling".format(nr)
+            print(ps)
+            if ps == "nps":
+                ls = '--'
+                label = "{0} reals, no prior scaling".format(nr)
+            c = cd[nr]
+            ax.plot(df.total_runs,df.loc[:,"mean"],ls=ls,color=c,lw=1.0,marker='.',ms=4,label=label)
+            #[ax.plot(df.total_runs,df.iloc[:,i],ls=ls,color=c,lw=0.05) for i in range(5,df.shape[1])]
+            if ps == "ps":
+                df.iloc[-1,5:].hist(ax=ad[nr],facecolor=c,alpha=0.5,bins=10,
+                                    normed=True,edgecolor="none")
+            else:
+                df.iloc[-1, 5:].hist(ax=ad[nr], bins=10, normed=True,hatch='/////',
+                                     edgecolor=c,facecolor="none")
 
-        c = cd[nr]
-        ax.plot(df.total_runs,df.loc[:,"mean"],ls=ls,color=c,lw=1.0,marker='.',label=label)
-        #[ax.plot(df.total_runs,df.iloc[:,i],ls=ls,color=c,lw=0.05) for i in range(5,df.shape[1])]
-    ax.set_xlim(0,ax.get_xlim()[1])
+    #ax.set_xlim(0,ax.get_xlim()[1])
+    ax.set_xlim(0,pst.npar_adj)
     ax.set_ylim(0, ax.get_ylim()[1])
     ax.set_xlabel("model runs")
     ax.set_ylabel("mean objective function ($\phi$)")
     ax.grid()
     ax.legend()
+    ax.set_title('A)',loc="left")
+    for a,t in zip([ax1,ax2,ax3],['B)','C)','D)']):
+        a.set_yticks([])
+        a.set_xlabel('$\phi$')
+        a.set_title(t,loc='left')
+        a.set_xlim(0,50)
+        a.grid()
+
     plt.tight_layout()
     plt.savefig("freyberg_phi.pdf")
 
@@ -106,11 +139,11 @@ def plot_phi():
 
 
 def plot_histograms():
-    pass
-
+    master_dirs = [d for d in os.listdir('.') if "master_" in d and "pestpp" not in d]
+    
 
 if __name__ == "__main__":
-    # run()
+    #run()
     #plot_domain()
-    plot_phi()
+    #plot_phi()
     plot_histograms()
