dc6c954e29cf7da6252e8d9a2a79f2da48f142f6
Jeremy White <jeremyw@vpn-49-191.gns.cri.nz>
2018-05-02 13:21:10 +1200

    updated travis_tests.py

--- benchmarks/ies_test.py
+++ benchmarks/ies_test.py
@@ -4,6 +4,7 @@ import shutil
 import platform
 import numpy as np
 import pandas as pd
+import platform
 import matplotlib.pyplot as plt
 import pyemu
 
@@ -29,7 +30,12 @@ ies_vars = ["ies_par_csv", "ies_obs_csv", "ies_restart_obs_csv",
             "ies_use_approx", "ies_use_prior_scaling", "ies_reg_factor",
             "ies_lambda_mults", "ies_init_lambda","ies_include_base","ies_subset_size"]
 
-exe_path = os.path.join("..", "..", "..", "exe", "windows", "x64", "Release", "pestpp-ies.exe")
+if "windows" in platform.platform().lower():
+    exe_path = os.path.join("..", "..", "..", "exe", "windows", "x64", "Release", "pestpp-ies.exe")
+elif "darwin" in platform.platform().lower():
+    exe_path = os.path.join("..", "..", "..", "exe", "mac", "pestpp-ies")
+else:
+    exe_path = os.path.join("..", "..", "..", "exe", "linux", "pestpp-ies")
 
 noptmax = 3
 
@@ -702,8 +708,8 @@ def test_chenoliver():
 
     pyemu.helpers.start_slaves(template_d,exe_path,"pest.pst",num_slaves=20,
         master_dir=test_d,slave_root=model_d,port=4005)
-    df_full_obs = pd.read_csv(os.path.join(test_d,"pest.{0}.obs.csv.".format(noptmax)),index_col=0)
-    df_full_par = pd.read_csv(os.path.join(test_d,"pest.{0}.par.csv.".format(noptmax)),index_col=0)
+    df_full_obs = pd.read_csv(os.path.join(test_d,"pest.{0}.obs.csv".format(noptmax)),index_col=0)
+    df_full_par = pd.read_csv(os.path.join(test_d,"pest.{0}.par.csv".format(noptmax)),index_col=0)
 
     shutil.rmtree(test_d)
     pst.pestpp_options = {}
@@ -718,8 +724,8 @@ def test_chenoliver():
 
     pyemu.helpers.start_slaves(template_d,exe_path,"pest.pst",num_slaves=20,
         master_dir=test_d,slave_root=model_d,port=4005)
-    df_approx_obs = pd.read_csv(os.path.join(test_d,"pest.{0}.obs.csv.".format(noptmax)),index_col=0)
-    df_approx_par = pd.read_csv(os.path.join(test_d,"pest.{0}.par.csv.".format(noptmax)),index_col=0)
+    df_approx_obs = pd.read_csv(os.path.join(test_d,"pest.{0}.obs.csv".format(noptmax)),index_col=0)
+    df_approx_par = pd.read_csv(os.path.join(test_d,"pest.{0}.par.csv".format(noptmax)),index_col=0)
 
     ax = plt.subplot(211)
     ax2 = plt.subplot(212)
@@ -755,8 +761,8 @@ def test_chenoliver():
 
     pyemu.helpers.start_slaves(template_d,exe_path,"pest.pst",num_slaves=25,
         master_dir=test_d,slave_root=model_d,port=4005)
-    df_full_obs = pd.read_csv(os.path.join(test_d,"pest.{0}.obs.csv.".format(noptmax)),index_col=0)
-    df_full_par = pd.read_csv(os.path.join(test_d,"pest.{0}.par.csv.".format(noptmax)),index_col=0)
+    df_full_obs = pd.read_csv(os.path.join(test_d,"pest.{0}.obs.csv".format(noptmax)),index_col=0)
+    df_full_par = pd.read_csv(os.path.join(test_d,"pest.{0}.par.csv".format(noptmax)),index_col=0)
 
     shutil.rmtree(test_d)
     pst.pestpp_options = {}
@@ -771,8 +777,8 @@ def test_chenoliver():
 
     pyemu.helpers.start_slaves(template_d,exe_path,"pest.pst",num_slaves=25,
         master_dir=test_d,slave_root=model_d,port=4005)
-    df_approx_obs = pd.read_csv(os.path.join(test_d,"pest.{0}.obs.csv.".format(noptmax)),index_col=0)
-    df_approx_par = pd.read_csv(os.path.join(test_d,"pest.{0}.par.csv.".format(noptmax)),index_col=0)
+    df_approx_obs = pd.read_csv(os.path.join(test_d,"pest.{0}.obs.csv".format(noptmax)),index_col=0)
+    df_approx_par = pd.read_csv(os.path.join(test_d,"pest.{0}.par.csv".format(noptmax)),index_col=0)
 
     ax = plt.subplot(211)
     ax2 = plt.subplot(212)
