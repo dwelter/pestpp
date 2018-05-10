d83bb36023450b4fca7a02fb0c76848dfafc3c9e
Jeremy White <jeremyw@vpn-48-9.gns.cri.nz>
2018-05-11 11:56:27 +1200

    updated mac binaries

--- benchmarks/ies_10par_xsec/invest.py
+++ benchmarks/ies_10par_xsec/invest.py
@@ -12,8 +12,13 @@ def setup():
     pst = pyemu.Pst(os.path.join("master","pest.pst"))
     pst.pestpp_options = {}
     pst.control_data.noptmax = 3
-    pst.write(os.path.join("master","pest.pst"))
-    pyemu.os_utils.run("pestpp-ies pest.pst",cwd="master")
+    pst.write(os.path.join("template","pest_pareto.pst"))
+
+    nreal_per = 5
+
+
+    #pyemu.os_utils.run("pestpp-ies pest.pst /h :4004",cwd="master")
+    pyemu.os_utils.start_slaves("template","pestpp-ies","pest_pareto.pst",num_slaves=20,master_dir="master")
 
 if __name__ == "__main__":
     setup()
