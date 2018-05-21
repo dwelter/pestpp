bug fix in group draws - not using the right block size, added scaling factor for really small variances

benchmarks/ies_test.py
@@ -276,6 +276,75 @@ def rebase(model_d):
                              os.path.join(base_d, "{0}__{1}".
                                           format(os.path.split(test_d)[-1], compare_file)))
 
+
+def tenpar_narrow_range_test():
+    model_d = "ies_10par_xsec"
+    test_d = os.path.join(model_d, "master_narrow_test")
+    template_d = os.path.join(model_d, "test_template")
+    if not os.path.exists(template_d):
+        raise Exception("template_d {0} not found".format(template_d))
+    if os.path.exists(test_d):
+        shutil.rmtree(test_d)
+    shutil.copytree(template_d, test_d)
+    pst_name = os.path.join(test_d, "pest.pst")
+    pst = pyemu.Pst(pst_name)
+    par = pst.parameter_data
+    par.loc[:, "partrans"] = "fixed"
+    par.loc[:, "parubnd"] = 1.0e+10 #par.parval1 * 1.0001
+    par.loc[:, "parlbnd"] = 1.0e-10 #par.parval1 * 0.9999
+    par.loc[pst.par_names[:2], "partrans"] = "none"
+    #par.loc[pst.par_names[0],"pargp"] = "stage"
+
+    x = np.zeros((pst.npar_adj, pst.npar_adj)) + 1.0e-11
+    for i in range(pst.npar_adj):
+        x[i, i] = 5.0e-10
+    cov = pyemu.Cov(x, names=pst.adj_par_names)
+    cov.to_ascii(os.path.join(test_d, "prior.cov"))
+    num_reals = 5000
+    pe = pyemu.ParameterEnsemble.from_gaussian_draw(pst, cov, num_reals=num_reals, use_homegrown=True)
+    #pe.enforce()
+    #pe.to_csv(os.path.join(test_d,"pyemu_draws.csv"))
+
+    pst.control_data.noptmax = 0
+
+    pst.observation_data.loc[pst.nnz_obs_names,"weight"] *= 1.5
+
+    pst.pestpp_options = {}
+    pst.pestpp_options["ies_num_reals"] = num_reals
+    pst.pestpp_options["parcov_filename"] = "prior.cov"
+    pst.pestpp_options["ies_enforce_bounds"] = False
+    pst.pestpp_options["ies_include_base"] = False
+    pst.pestpp_options["ies_group_draws"] = False
+    pst.pestpp_options["ies_verbose_level"] = 3
+    pst.write(pst_name)
+    pyemu.helpers.run(exe_path + " pest.pst", cwd=test_d)
+
+    df = pd.read_csv(os.path.join(test_d, "pest.0.par.csv"), index_col=0)
+    df.columns = [c.lower() for c in df.columns]
+    p1, p2 = pst.adj_par_names
+    v1,v2 = pe.loc[:,p1].var(),df.loc[:,p1].var()
+    diff = np.abs(100 * ((v1 - v2) / v1))
+    print(v1,v2,diff)
+    assert diff < 1.0
+
+    pst.pestpp_options = {}
+    pst.pestpp_options["ies_num_reals"] = num_reals
+    pst.pestpp_options["parcov_filename"] = "prior.cov"
+    pst.pestpp_options["ies_enforce_bounds"] = False
+    pst.pestpp_options["ies_include_base"] = False
+    pst.pestpp_options["ies_group_draws"] = True
+    pst.pestpp_options["ies_verbose_level"] = 3
+    pst.write(pst_name)
+    pyemu.helpers.run(exe_path + " pest.pst", cwd=test_d)
+
+    df = pd.read_csv(os.path.join(test_d, "pest.0.par.csv"), index_col=0)
+    df.columns = [c.lower() for c in df.columns]
+    p1, p2 = pst.adj_par_names
+    v1, v2 = pe.loc[:, p1].var(), df.loc[:, p1].var()
+    diff = np.abs(100 * ((v1 - v2) / v1))
+    print(v1, v2, diff)
+    assert diff < 1.0
+
 def tenpar_full_cov_test():
     model_d = "ies_10par_xsec"
     test_d = os.path.join(model_d, "master_full_cov_test")
@@ -1061,7 +1130,7 @@ if __name__ == "__main__":
     #tenpar_full_cov_test()
     #test_freyberg_full_cov_reorder()
     #test_freyberg_full_cov_reorder_run()
-    test_freyberg_full_cov()
+    #test_freyberg_full_cov()
     
     #test_synth()
     #test_10par_xsec()
@@ -1071,6 +1140,7 @@ if __name__ == "__main__":
     #compare_pyemu()
     #tenpar_subset_test()
     #tenpar_full_cov_test()
+    tenpar_narrow_range_test()
     #test_freyberg_ineq()
     
     # # invest()